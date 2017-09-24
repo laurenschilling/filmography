e4201a50d5c40408c2fb97215fb1cf6423ac0487
laurenschilling <lauren.hannah@hotmail.com>
2017-09-24 17:54:34 +1000

    Added loop to match movie genre ids with the genre name
    Added loop to match movie genre ids with the genre name, and display the genre name in the pop up div. Need to fix how it's displayed though – at the moment there's no spaces or commas between the genres.

--- scripts/app.js
+++ scripts/app.js
@@ -28,8 +28,52 @@ function movieController($scope, $http) {
         // if there is a release date, only show release year
         if ( movie.release_date ) {
 	        var yr = movie.release_date.substring(0, 4);
-	        movie.release_date = yr;
+			$('.yr').append(yr);
         }
-    };
+        
+        // if there is a genre id, find the genre name
+        if ( movie.genre_ids ) {
+	
+			var ids = movie.genre_ids;
+			
+			var genres = [
+				{ id: 28, name: 'Action' },
+				{ id: 12, name: 'Adventure' },
+				{ id: 16, name: 'Animation' },
+				{ id: 35, name: 'Comedy' },
+				{ id: 80, name: 'Crime' },
+				{ id: 99, name: 'Documentary' },
+				{ id: 18, name: 'Drama' },
+				{ id: 10751, name: 'Family' },
+				{ id: 14, name: 'Fantasy' },
+				{ id: 36, name: 'History' },
+				{ id: 27, name: 'Horror' },
+				{ id: 10402, name: 'Music' },
+				{ id: 9648, name: 'Mystery' },
+				{ id: 10749, name: 'Romance' },
+				{ id: 878, name: 'Science Fiction' },
+				{ id: 10770, name: 'TV Movie' },
+				{ id: 53, name: 'Thriller' },
+				{ id: 10752, name: 'War' },
+				{ id: 37, name: 'Western' }
+			]
+			
+			// match the movie ids with the genre names
+			for (var j = 0; j < genres.length; j++) {
 
+				for (var i = 0; i < ids.length; i++) {
+					if ( ids[i] == genres[j].id) {
+						console.log('Match: ' + ids[i] + ' and ' + genres[j].id);
+						
+						var currGenres = [];
+						currGenres = [(genres[j].name)];
+						console.log(currGenres);
+						
+						// ** need to fix how this is appended
+						$('.genre-ids').append(currGenres);
+					}
+				}
+	    	};
+		}
+	}
 }
